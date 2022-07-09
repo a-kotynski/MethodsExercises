6b6afa7fdfd16af55c2337426c7a143842fc7ae5
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-07-09 10:03:14 +0200

    Add project files.

---
+++ MethodsExercises/Calculator.cs
@@ -0,0 +1,16 @@
+﻿//https://www.udemy.com/course/csharp-intermediate-classes-interfaces-and-oop/learn/lecture/2484612#content
+namespace MethodsExercises
+{
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
---
+++ MethodsExercises/Point.cs
@@ -0,0 +1,31 @@
+﻿namespace MethodsExercises
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+            {
+                throw new ArgumentNullException("newLocation"); // throw signals an occurence of an exception
+            }
+            Move(newLocation.X, newLocation.Y);
+
+
+
+            //this.X = newLocation.X;
+            //this.Y = newLocation.Y;
+        }
+    }
+}
